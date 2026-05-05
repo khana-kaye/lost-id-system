f7fc85e159dd8a4a232be293dea54a0aac3ef8b1
khana-kaye <khanakaye4@gmail.com>
2026-05-05 03:18:14 +0300

    add create user endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import IDRecordViewSet
 from . import views
-from .views import create_user
+from .views import login, create_user
 
 router = DefaultRouter()
 router.register(r'ids', IDRecordViewSet)
@@ -10,5 +10,5 @@ router.register(r'ids', IDRecordViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path("login/", views.login),
-    path("api/create-user/", views.create_user),
+    path("create-user/", create_user),
 ]
