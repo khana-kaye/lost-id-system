381d21be153529b5b315b2bc44bdda387a5de7d7
khana-kaye <khanakaye4@gmail.com>
2026-05-10 16:42:55 +0300

    frontend issues

--- frontend/src/pages/bank/BankDashboard.jsx
+++ frontend/src/pages/bank/BankDashboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+//import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PageLayout from "../../components/PageLayout";
 import { theme } from "../../theme";
