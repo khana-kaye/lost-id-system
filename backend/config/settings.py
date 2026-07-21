141008352c05698468a1f70471600bff7d16f7e2
khana-kaye <khanakaye4@gmail.com>
2026-07-22 00:49:24 +0300

    Enable debug temporarily

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,10 +28,12 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get(
-    'DEBUG',
-    'True' if not os.environ.get('DATABASE_URL') else 'False',
-).lower() == 'true'
+# DEBUG = os.environ.get(
+#     'DEBUG',
+#     'True' if not os.environ.get('DATABASE_URL') else 'False',
+# ).lower() == 'true'
+
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
